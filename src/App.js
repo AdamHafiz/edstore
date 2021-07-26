522179f9d58eadb4920f990bd77e6f99146cbf9c
AdamHafiz <adamhafizabhadi78@gmail.com>
2021-07-26 18:50:11 +0800

    my new project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Homepage from "./home.component";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;

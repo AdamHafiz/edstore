5473c552e8e3137b08d73306ada11fb6acea9d86
AdamHafiz <adamhafizabhadi78@gmail.com>
2021-07-27 23:30:36 +0800

    update menu item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
 import TestComp from "./TestComp";
 
 function App() {
   return (
     <div>
-      <Homepage />
-
-      <TestComp />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={TestComp} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,28 +13,33 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl: "https://thumbs.dreamstime.com/b/hat-18089956.jpg",
           id: 1,
+          linkUrl: "hats",
         },
         {
           title: "jackets",
           imageUrl: "https://thumbs.dreamstime.com/b/hat-18089956.jpg",
           id: 2,
+          linkUrl: "",
         },
         {
           title: "sneakers",
           imageUrl: "https://thumbs.dreamstime.com/b/hat-18089956.jpg",
           id: 3,
+          linkUrl: "",
         },
         {
           title: "women",
           imageUrl: "https://thumbs.dreamstime.com/b/hat-18089956.jpg",
           size: "large",
           id: 4,
+          linkUrl: "",
         },
         {
           title: "men",
           imageUrl: "https://thumbs.dreamstime.com/b/hat-18089956.jpg",
           size: "large",
           id: 5,
+          linkUrl: "",
         },
       ],
     };
@@ -43,8 +48,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
